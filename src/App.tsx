f94b61c857efbb88936066fa3ef5d6f44288dc1a
asosori <tsukimiteritama@gmail.com>
2019-08-02 20:54:42 +0900

    finish building an environment

---
+++ src/App.tsx
@@ -0,0 +1,10 @@
+import React, { Component } from 'react';
+import { Text } from 'react-native';
+
+class App extends Component {
+  render() {
+    return <Text>helloyfd</Text>;
+  }
+}
+
+export default App;
