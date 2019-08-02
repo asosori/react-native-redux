b642592fd9c340bcab48edddee0d8d1f1b2b9880
asosori <tsukimiteritama@gmail.com>
2019-08-02 20:54:42 +0900

    finish building an environment

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+import { Text, View, Button } from 'react-native';
+//import { Navigation } from 'react-native-navigation';
+
+class App extends Component {
+  render() {
+    return <Text>fsd</Text>;
+  }
+}
+
+export default App;
