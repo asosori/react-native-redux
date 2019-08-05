4ad420eefe127f777fb6e9af5e1889b59e9cf755
asosori <tsukimiteritama@gmail.com>
2019-08-05 19:05:08 +0900

    introduce redux and edit screen

--- screens/Create.js
+++ screens/Create.js
@@ -1,10 +1,54 @@
-import React, { Component } from 'React';
+import React, { Fragment, Component } from 'React';
 import { Text } from 'react-native';
+import Icon from 'react-native-vector-icons/FontAwesome';
+import { Input, Button } from 'react-native-elements';
+import { connect } from 'react-redux';
 
-export default class Create extends Component {
+class Create extends Component {
+
+  create_review(){
+    const { place, station, comment } = this.props.restaurantForm;
+    this.props.createReview({ place, station, comment });
+  }
+
   render(){
     return(
-    <Text>fadfdas</Text>
+      <Fragment>
+        <Text>fadfdas</Text>
+
+        <Input
+          placeholder='場所'
+          leftIcon={
+            <Icon
+              name='user'
+              size={24}
+              color='black'
+            />
+          }
+        />
+
+        <Input
+          placeholder='最寄駅'
+          shake={true}
+        />
+
+        <Input
+          placeholder='コメント'
+          leftIcon={{ type: 'font-awesome', name: 'chevron-left' }}
+        />
+
+        <Button 
+          title="create a review"
+          onPress={this.create_review.bind(this)}
+        />
+
+    </Fragment>
     )
   }
 }
+
+const mapStateToProps = state => {
+  restaurantForm: state.restaurantForm
+};
+
+export default connect(mapStateToProps, { createReview })(Create);
--- screens/Home.js
+++ screens/Home.js
@@ -19,7 +19,7 @@ export default class HomeScreen extends Component {
               component: {
                 name: 'CreateForm'
               }
-            })            
+            })    
           }}
         />
       </Fragment>
