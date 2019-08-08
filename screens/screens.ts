25cb9154abff7932481c1658fc86b305cb2440bd
asosori <tsukimiteritama@gmail.com>
2019-08-08 18:38:41 +0900

    introduce redux and store and reducer

--- screens/screens.ts
+++ screens/screens.ts
@@ -1,8 +1,20 @@
 import { Navigation } from 'react-native-navigation';
+import store from '../src/store';
+import { Provider } from 'react-redux';
 import HomeScreen from './Home';
 import CreateScreen from './Create';
 
 export const registerScreens = () => {
-  Navigation.registerComponent('HomeScreen', () => HomeScreen);
-  Navigation.registerComponent('CreateForm', () => CreateScreen);
+  Navigation.registerComponentWithRedux(
+    'HomeScreen',
+    () => HomeScreen,
+    Provider,
+    store
+  );
+  Navigation.registerComponentWithRedux(
+    'CreateForm',
+    () => CreateScreen,
+    Provider,
+    store
+  );
 };
--- src/App.ts
+++ src/App.ts
@@ -1,5 +1,7 @@
 import { Navigation } from 'react-native-navigation';
 import { registerScreens } from '../screens/screens';
+import ReduxThunk from 'redux-thunk';
+
 registerScreens();
 
 Navigation.events().registerAppLaunchedListener(() => {
