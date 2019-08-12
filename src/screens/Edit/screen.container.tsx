53f5453a29e9c2d31520d1f4a4f48cef727c9ee7
asosori <tsukimiteritama@gmail.com>
2019-08-12 19:13:02 +0900

    introduce lodash and put restaurant_form in edit display

--- src/screens/Edit/screen.container.tsx
+++ src/screens/Edit/screen.container.tsx
@@ -3,7 +3,9 @@ import React, { Fragment, Component } from 'React';
 import { Input, Button } from 'react-native-elements';
 import RestaurantForm from '../RestaurantForm';
 import { Dispatch, Action } from 'redux';
-// import restaurantUpdate from ''
+import restaurantFormUpdate from '../../actions/restaurantFormUpdate';
+import restaurantSave from '../../actions/restaurantSave';
+import _ from 'lodash';
 
 export interface Props {
   restaurant: {
@@ -11,41 +13,50 @@ export interface Props {
     station: string;
     comment: string;
   };
-  //restaurantUpdate:
+  restaurantSave: () => void;
+  restaurantFormUpdate: (props: { prop: string; value: string }) => void;
 }
 
-export default class EditScreen extends Component<Props> {
+class EditScreen extends Component<Props> {
   // onPressUpdate(){
-  //   this.props.restaurantUpdate();
+  //   this.props.restaurantSave();
   // }
+  constructor(props: Props) {
+    super(props);
+    _.each(this.props.restaurant, (value, prop) => {
+      this.props.restaurantFormUpdate({ prop, value });
+    });
+  }
 
   render() {
     return (
       <Fragment>
-        <Input value={this.props.restaurant.name} placeholder="お店の名前" />
+        <RestaurantForm {...this.props} />
+        {/* <Input value={this.props.restaurant.name} placeholder="お店の名前" />
 
         <Input value={this.props.restaurant.station} placeholder="最寄駅" />
 
-        <Input value={this.props.restaurant.comment} placeholder="コメント" />
+        <Input value={this.props.restaurant.comment} placeholder="コメント" /> */}
 
-        <Button
-          title="更新する"
-          onPress={() => console.log('pushed the update button')}
-        />
+        <Button title="更新する" onPress={() => this.props.restaurantSave()} />
       </Fragment>
     );
   }
 }
 
-// const mapStateToProps = (state: any) => {
-//   return { restaurantForm: state.restaurantForm };
-// };
+const mapStateToProps = (state: any) => {
+  return { restaurantForm: state.restaurantForm };
+};
 
-// const mapDispatchToProps = (dispatch: Dispatch<Action>) => {
-//   return { restaurantUpdate: () => dispatch(restaurantUpdate()) };
-// };
+const mapDispatchToProps = (dispatch: Dispatch<Action>) => {
+  return {
+    restaurantSave: () => restaurantSave(),
+    restaurantFormUpdate: (props: { prop: string; value: string }) =>
+      dispatch(restaurantFormUpdate(props))
+  };
+};
 
-// export default connect(
-//   mapStateToProps,
-//   mapDispatchToProps
-// )(EditScreen);
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(EditScreen);
